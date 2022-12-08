d383718e7585e65d6d329cf6d004bbd1ccc081a3
Tom Lanser <tom@devv.nl>
2022-12-09 00:21:08 +0100

    feat: Added more radio stations
    Signed-off-by: Tom Lanser <tom@devv.nl>

--- channels/channels.go
+++ channels/channels.go
@@ -1,18 +1,21 @@
 package channels
 
 type RadioChannel struct {
-	Name        string
-	Url         string
-	SnowflakeId string
+	Name               string
+	Url                string
+	DiscordSnowflakeId string
 }
 
 var (
 	RadioChannels = []RadioChannel{
-		{Name: "SlamFM", Url: "https://stream.slam.nl/slam_mp3", SnowflakeId: "slamfm"},
+		{Name: "SlamFM", Url: "https://stream.slam.nl/slam_mp3", DiscordSnowflakeId: "slamfm"},
 		{Name: "Veronica", Url: "https://playerservices.streamtheworld.com/api/livestream-redirect/VERONICA.mp3"},
 		{Name: "NPO Radio 1", Url: "https://icecast.omroep.nl/radio1-bb-mp3"},
-		{Name: "NPO Radio 2", Url: "https://icecast.omroep.nl/radio2-bb-mp3", SnowflakeId: "nporadio2"},
-		{Name: "NPO 3FM", Url: "https://icecast.omroep.nl/3fm-bb-mp3", SnowflakeId: "3fm"},
+		{Name: "NPO Radio 2", Url: "https://icecast.omroep.nl/radio2-bb-mp3", DiscordSnowflakeId: "nporadio2"},
+		{Name: "NPO Radio 2 Soul & Jazz", Url: "https://icecast.omroep.nl/radio6-bb-mp3", DiscordSnowflakeId: "nporadio2"},
+		{Name: "NPO 3FM", Url: "https://icecast.omroep.nl/3fm-bb-mp3", DiscordSnowflakeId: "3fm"},
 		{Name: "NPO FunX", Url: "https://icecast.omroep.nl/funx-bb-mp3"},
+		{Name: "538", Url: "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO538.mp3", DiscordSnowflakeId: "538"},
+		{Name: "538 Verrückte Stunden", Url: "https://playerservices.streamtheworld.com/api/livestream-redirect/TLPSTR21.mp3", DiscordSnowflakeId: "538"},
 	}
 )
